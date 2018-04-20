ab99e71e3d2603c0b96356ed31470370ff2d762f
Hatem-D <hdourai@gmail.com>
2018-04-20 23:30:37 +0200

    merge du vendredi soir

--- init.inc.php
+++ init.inc.php
@@ -3,7 +3,7 @@
 try{
 
  global $bdd;
- $bdd = new PDO('mysql:host=localhost; dbname=troc','root', 'root', array(PDO::ATTR_ERRMODE =>PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+ $bdd = new PDO('mysql:host=localhost; dbname=troc','root', '', array(PDO::ATTR_ERRMODE =>PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
 }
 
